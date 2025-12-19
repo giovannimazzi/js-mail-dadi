dba7dac0f395cc4b5ad094fbb7b58d37e69f6b09
giovannimazzi <gioelor@libero.it>
2025-12-19 15:52:06 +0100

    mail: creata lista invitati

---
+++ main.js
@@ -0,0 +1,26 @@
+// # MAIL
+
+//lista invitati
+const guestList = [
+  "manuel_aliquo@gmail.com",
+  "denis_balestro@gmail.com",
+  "michele_brignola@gmail.com",
+  "antonio_cappellano@gmail.com",
+  "gabriele_chiocca@gmail.com",
+  "valeria_cristiano@gmail.com",
+  "gabriela_tumfart@gmail.com",
+  "michele_ferri@gmail.com",
+  "mattia_galasso@gmail.com",
+  "brayan_genovese@gmail.com",
+  "sirio_ghiringhelli@gmail.com",
+  "manuel_gozzi@gmail.com",
+  "jasmin_hegic@gmail.com",
+  "bruno_maiolo@gmail.com",
+  "giancarlo_manzo@gmail.com",
+  "giovanni_mazzi@gmail.com",
+  "leonardo_monti@gmail.com",
+  "edoardo_nicora@gmail.com",
+  "salvatore_raia@gmail.com",
+  "andrea_salvi@gmail.com",
+  "marco_venerando@gmail.com",
+];
